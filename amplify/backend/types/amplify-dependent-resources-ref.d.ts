ffc356cb584b6b3413c6bd26550f16adc6f9aa05
AMAN <g1aman.singh.2575@gmail.com>
2021-12-03 11:42:25 +0530

    first commit

---
+++ amplify/backend/types/amplify-dependent-resources-ref.d.ts
@@ -0,0 +1,26 @@
+export type AmplifyDependentResourcesAttributes = {
+    "api": {
+        "amplifynext": {
+            "GraphQLAPIKeyOutput": "string",
+            "GraphQLAPIIdOutput": "string",
+            "GraphQLAPIEndpointOutput": "string"
+        }
+    },
+    "auth": {
+        "amplifynext93d15ae7": {
+            "IdentityPoolId": "string",
+            "IdentityPoolName": "string",
+            "UserPoolId": "string",
+            "UserPoolArn": "string",
+            "UserPoolName": "string",
+            "AppClientIDWeb": "string",
+            "AppClientID": "string"
+        }
+    },
+    "storage": {
+        "projectimages": {
+            "BucketName": "string",
+            "Region": "string"
+        }
+    }
+}
